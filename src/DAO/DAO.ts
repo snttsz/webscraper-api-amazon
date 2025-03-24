1a66ef0ccadeebdea0199eacba22bb47a7449ce2
Glenda Santana <glendasantana2099@gmail.com>
2025-03-24 10:34:48 -0300

    src/DAO/DAO -> Documenta o módulo

--- src/DAO/DAO.ts
+++ src/DAO/DAO.ts
@@ -2,6 +2,14 @@ import { DynamoDB } from 'aws-sdk';
 
 import { Logger } from '../utils/logger';
 
+
+/**
+ * Classe abstrata DAO<T>
+ * 
+ * Esta classe fornece uma implementação genérica para interagir com o DynamoDB. 
+ * Ela fornece métodos básicos para realizar operações CRUD (criar, ler, atualizar, deletar e listar) 
+ * em uma tabela do DynamoDB, além de facilitar a criação de tabelas e aguardar a ativação da tabela.
+ */
 export abstract class DAO<T>
 {
     protected dynamoDB: DynamoDB.DocumentClient;
@@ -11,11 +19,12 @@ export abstract class DAO<T>
     protected ultimo_id : number = 0;
 
     /**
+     * Construtor da classe DAO.
      * 
+     * Inicializa a instância do DynamoDB, o logger e obtém o último id da tabela.
      * 
-     * @param
-     * @param 
-     * @returns 
+     * @param tableName Nome da tabela do DynamoDB que será manipulada
+     * @param log Instância de Logger para registrar as operações realizadas
      */
     constructor(tableName: string, log: Logger)
     {
@@ -29,18 +38,23 @@ export abstract class DAO<T>
         });
     }
 
+    /**
+     * Retorna o último id utilizado na tabela.
+     * 
+     * @returns O último id utilizado para inserção de itens na tabela.
+     */
     public getUltimoId() : number
     {
         return this.ultimo_id;
     }
 
     /**
+     * Cria a tabela no DynamoDB se não existir.
      * 
+     * A tabela terá uma chave primária baseada no atributo "id", que será do tipo numérico.
      * 
-     * @param
-     * @param 
-     * @returns 
-     */ 
+     * @returns Uma Promise que será resolvida quando a tabela for criada ou se já existir.
+     */
     async criarTabela() : Promise<void>
     {
         const params: DynamoDB.CreateTableInput = 
@@ -81,20 +95,22 @@ export abstract class DAO<T>
     }
     
     /**
+     * Método abstrato para criar um novo item na tabela.
      * 
+     * Este método deve ser implementado nas classes filhas para definir como criar um item específico.
      * 
-     * @param
-     * @param 
-     * @returns 
+     * @param item O item a ser inserido na tabela
+     * @returns Uma Promise que será resolvida após a criação do item
      */
     abstract criar(item: T) : Promise<void>;
 
     /**
+     * Cria múltiplos itens na tabela a partir de uma lista.
      * 
+     * Este método chama o método `criar()` para cada item da lista.
      * 
-     * @param
-     * @param 
-     * @returns 
+     * @param itens A lista de itens a serem inseridos
+     * @returns Uma Promise que será resolvida após a criação de todos os itens
      */
     async criarDeLista(itens: T[]): Promise<void> 
     {
@@ -105,11 +121,10 @@ export abstract class DAO<T>
     }
 
     /**
+     * Lê um item da tabela com base no seu id.
      * 
-     * 
-     * @param
-     * @param 
-     * @returns 
+     * @param id O id do item a ser lido
+     * @returns O item encontrado, ou null se o item não for encontrado
      */
     async ler(id: string) : Promise<T | null>
     {
@@ -124,20 +139,21 @@ export abstract class DAO<T>
     }
 
     /**
+     * Método abstrato para atualizar um item na tabela.
      * 
+     * Este método deve ser implementado nas classes filhas para definir como atualizar um item específico.
      * 
-     * @param
-     * @param 
-     * @returns 
+     * @param id O id do item a ser atualizado
+     * @param updatedItem O item com as atualizações a serem aplicadas
+     * @returns Uma Promise que será resolvida após a atualização do item
      */
     abstract atualizar(id: string, updatedItem: T): Promise<void>;
 
     /**
+     * Deleta um item da tabela com base no seu id.
      * 
-     * 
-     * @param
-     * @param 
-     * @returns 
+     * @param id O id do item a ser deletado
+     * @returns Uma Promise que será resolvida após a exclusão do item
      */
     async deletar(id: string): Promise<void> 
     {
@@ -150,11 +166,9 @@ export abstract class DAO<T>
     }
 
     /**
+     * Lista todos os itens presentes na tabela.
      * 
-     * 
-     * @param
-     * @param 
-     * @returns 
+     * @returns Uma Promise que resolve com a lista de itens da tabela
      */
     async listar() : Promise<T[]>
     {
@@ -167,6 +181,13 @@ export abstract class DAO<T>
         return result.Items as T[];
     }
 
+    /**
+     * Aguarda até que a tabela esteja ativa.
+     * 
+     * Este método é útil para garantir que a tabela tenha sido criada e esteja disponível para operações.
+     * 
+     * @returns Uma Promise que será resolvida quando a tabela estiver ativa
+     */
     async esperarTabelaEstarAtiva(): Promise<void> 
     {
         this.log.getLogger().info(`[DynamoDB] waitForTableActive() -> Aguardando a tabela ${this.tableName} ficar ativa...`);
